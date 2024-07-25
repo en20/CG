2c89972c74cb7346e6701314609d3fb8015ea3fd
en20 <enzolozano90@gmail.com>
2024-07-25 14:21:52 -0300

    Add main files to the project

---
+++ CMAKE-SDL2-Skeleton-master/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt install git build-essential pkg-config cmake cmake-data libsdl2-dev libsdl2-gfx-dev
+
+sudo apt install libsdl2-image-dev libsdl2-ttf-dev libsdl2-net-dev libsdl2-mixer-dev
+
